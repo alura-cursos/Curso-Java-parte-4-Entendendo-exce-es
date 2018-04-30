cf11ad201c7ee33e0dbd79682a3f7f45bf56b44d
Andre Chaves <andrechavesg@gmail.com>
2018-04-29 22:39:30 -0300

    capitulo1

---
+++ java-pilha/src/Fluxo.java
@@ -0,0 +1,22 @@
+public class Fluxo {
+
+    public static void main(String[] args) {
+        System.out.println("Ini do main");
+        metodo1();
+        System.out.println("Fim do main");
+    }
+
+    private static void metodo1() {
+        System.out.println("Ini do metodo1");
+        metodo2();
+        System.out.println("Fim do metodo1");
+    }
+
+    private static void metodo2() {
+        System.out.println("Ini do metodo2");
+        for(int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Fim do metodo2");        
+    }
+}

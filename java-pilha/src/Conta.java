c616d05497a65a14c2fc1ac49cccab4d9fb9f5ad
Andre Chaves <andrechavesg@gmail.com>
2018-04-29 22:57:37 -0300

    capitulo2

---
+++ java-pilha/src/Conta.java
@@ -0,0 +1,5 @@
+public class Conta {
+
+    void deposita() {}
+
+}
--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -2,7 +2,13 @@ public class Fluxo {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
-        metodo1();
+        try {
+            metodo1();
+        } catch(ArithmeticException | NullPointerException ex) {
+            String msg = ex.getMessage();
+            System.out.println("Exception " + msg);
+            ex.printStackTrace();
+        }
         System.out.println("Fim do main");
     }
 
@@ -16,6 +22,8 @@ public class Fluxo {
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
+            Conta c = null;
+            c.deposita();
         }
         System.out.println("Fim do metodo2");        
     }
